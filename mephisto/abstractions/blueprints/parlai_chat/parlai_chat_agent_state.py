4435185a70e42cc9f56de8b5feb2d3f8d1432e7d
Eric Smith <EricMichaelSmith@users.noreply.github.com>
2022-08-19 10:36:33 -0400

    Update parlai_chat_agent_state.py

--- mephisto/abstractions/blueprints/parlai_chat/parlai_chat_agent_state.py
+++ mephisto/abstractions/blueprints/parlai_chat/parlai_chat_agent_state.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
-import datetime
+from datetime import datetime
 from typing import List, Optional, Dict, Any, Tuple, TYPE_CHECKING
 from mephisto.abstractions._subcomponents.agent_state import (
     AgentState,
